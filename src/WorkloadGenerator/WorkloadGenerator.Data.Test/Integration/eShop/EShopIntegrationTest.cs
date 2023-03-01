d6480deb0436633f032dd9b6674f946a4cc3b899
kct949 <kct949@alumni.ku.dk>
2023-03-01 15:04:53 +0100

    move the integration test files

---
+++ src/WorkloadGenerator/WorkloadGenerator.Data.Test/Integration/eShop/EShopIntegrationTest.cs
@@ -0,0 +1,10 @@
+namespace WorkloadGenerator.Data.Test.Integration.eShop;
+
+public class EShopIntegrationTest
+{
+    [Test] 
+    public void ParseValidTransactions()
+    {
+        // File    
+    }
+}
