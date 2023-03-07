96efbdbfe5ea3e479a3ec273efd5dca8ecf53145
Philipp Theyssen <p.theyssen@gmail.com>
2023-03-07 12:27:17 +0100

    Add option to specify path for json argument in operations.
    Also fixed small issues discussed in PR.
    
    Operation files now support to access a path on a argument in the
    following way:
    
    {{@@items[0].price@@}}

--- src/WorkloadGenerator/WorkloadGenerator.Data/Models/Generator/GuidGenerator.cs
+++ src/WorkloadGenerator/WorkloadGenerator.Data/Models/Generator/GuidGenerator.cs
@@ -2,6 +2,8 @@ namespace WorkloadGenerator.Data.Models.Generator;
 
 public class GuidGenerator : GeneratorBase, IGenerator
 {
+    public GeneratorType Type => GeneratorType.Guid;
+
     public object Next()
     {
         throw new NotImplementedException();
